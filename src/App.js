bf8082bb15b5156233fccf3db40d4a567eabf985
ivanrasgido <ivanrasgido19@gmail.com>
2021-04-21 14:30:48 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline'; 
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
