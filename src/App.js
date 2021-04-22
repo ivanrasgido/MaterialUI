35c21614affd7617e94e95951c8b2f7c3caba79e
ivanrasgido <ivanrasgido19@gmail.com>
2021-04-22 11:42:33 -0300

    Setup landing-page

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Container, CssBaseline } from '@material-ui/core';
+import GridLayout from './GridLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+
+      <Container>
+        <main>
+          <GridLayout />
+        </main>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GridLayout.js
@@ -0,0 +1,15 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+
+export default function GridLayout(){
+    return(
+        <Grid container>
+            <Grid item>
+
+            </Grid>
+            <Grid item>
+
+            </Grid>
+        </Grid>
+    );
+}
